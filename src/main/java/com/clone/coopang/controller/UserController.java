8ae51ee944fd1583ca4b6748eb152d9028e88f9f
whwjdan <whwjdan@naver.com>
2022-02-18 17:34:15 +0900

    @Controller -> @RestController 변경

--- src/main/java/com/clone/coopang/controller/UserController.java
+++ src/main/java/com/clone/coopang/controller/UserController.java
@@ -1,22 +1,26 @@
 package com.clone.coopang.controller;
 
 import com.clone.coopang.network.request.UserApiRequest;
+import com.clone.coopang.network.response.UserApiResponse;
 import com.clone.coopang.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    private UserController(UserService userService){
-        this.userService = userService;
-    }
-
     @PostMapping("/user/join")
     public void save(@RequestBody UserApiRequest userApiRequest){
+
         userService.createUser(userApiRequest);
     }
 
