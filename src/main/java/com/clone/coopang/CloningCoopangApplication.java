ace2443266dff8df5358d7fdccf72caee2f27444
whwjdan <whwjdan@naver.com>
2022-02-18 13:06:28 +0900

    spring 프로젝트 추가

---
+++ src/main/java/com/clone/coopang/CloningCoopangApplication.java
@@ -0,0 +1,13 @@
+package com.clone.coopang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloningCoopangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloningCoopangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clone/coopang/CloningCoopangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.coopang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloningCoopangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
