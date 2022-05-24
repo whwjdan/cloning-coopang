aaf80ebb622a73f6af7513b61bf2e14eabf1ba9d
whwjdan <whwjdan@naver.com>
2022-05-24 23:24:26 +0900

    [#13] refactor: Swagger 2 버전 오류로 인한 버전 3로 변경 및 Config 수정

--- src/main/java/com/clone/coopang/config/SwaggerConfig.java
+++ src/main/java/com/clone/coopang/config/SwaggerConfig.java
@@ -2,12 +2,18 @@ package com.clone.coopang.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableWebMvc
@@ -20,11 +26,13 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(swaggerInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
-                //.paths(PathSelectors.ant("/api/**")) // 그중 /api/** 인 URL들만 필터링
-                .build();
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
     }
 
 }
